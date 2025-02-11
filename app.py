51c39dbb2462e633469cc498a9ac81986e47ab89
dannyschaffer <schaffer.danny@gmail.com>
2025-02-11 09:26:34 +0200

    sql fix

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ if not db_url:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     db_path = os.path.join(BASE_DIR, 'orders.db')
     db_url = 'sqlite:///' + db_path
+else:
+    # Add SSL mode for PostgreSQL connection
+    db_url = db_url + "?sslmode=require"
+    
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
